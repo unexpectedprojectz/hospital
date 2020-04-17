14d87e465e3a090fe46d49df92f8e2ae15cd6e6e
unexpectedprojectz <alexjimenezcamacho76@gmail.com>
2020-04-17 18:06:30 +0200

    New

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,8 @@
 {
     'name': 'Hospital',
     'version': '12.0.1.0.0',
-    'author': 'AlexDani',
-    'maintainer': 'AlexDani',
+    'author': 'AlexDaniAdam',
+    'maintainer': 'AlexDaniAdam',
     'website': 'http://www.copernic.cat',
     'license': 'AGPL-3',
     'category': 'Extra Tools',
